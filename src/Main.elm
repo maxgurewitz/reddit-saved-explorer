027a811210e813258e1db08d61c18eaf72d862d3
maxgurewitz <maxthegeek1@gmail.com>
2018-10-07 18:47:51 -0700

    dev authorize link

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,13 @@
 module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
 import Browser
-import Html exposing (Html, div, img, text)
+import Dict
+import Html exposing (Html, a, div, img, text)
 import Html.Attributes as Attr
+import Random
+import Random.Char
+import Random.String
+import Url.Builder
 
 
 
@@ -11,13 +16,16 @@ import Html.Attributes as Attr
 
 type alias Model =
     { publicPath : String
+    , redditAuthState : Maybe String
     }
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { publicPath = flags.publicPath }
-    , Cmd.none
+    ( { publicPath = flags.publicPath
+      , redditAuthState = Maybe.Nothing
+      }
+    , Random.generate GenerateRedditAuthState (Random.String.string 15 Random.Char.english)
     )
 
 
@@ -27,6 +35,7 @@ init flags =
 
 type Msg
     = NoOp
+    | GenerateRedditAuthState String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -35,13 +44,61 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
+        GenerateRedditAuthState randomString ->
+            ( { model | redditAuthState = Maybe.Just randomString }, Cmd.none )
+
 
 
 -- VIEW
 
 
+buildAuthLinkQueryString randomString =
+    let
+        -- FIXME condition on environment
+        redirectUri =
+            "http://localhost:3000"
+
+        clientId =
+            "Lf7PiyAEaoSp6Q"
+
+        params =
+            [ ( "client_id", clientId )
+            , ( "response_type", "code" )
+            , ( "state", randomString )
+            , ( "duration", "temporary" )
+            , ( "scope", "history" )
+            , ( "redirect_uri", redirectUri )
+            ]
+    in
+    params
+        |> List.map (\( key, val ) -> Url.Builder.string key val)
+        |> Url.Builder.toQuery
+
+
+
+-- https://github.com/reddit-archive/reddit/wiki/OAuth2
+-- https://github.com/not-an-aardvark/snoowrap
+-- https://www.reddit.com/dev/api/oauth#GET_user_{username}_saved
+
+
+authUrl =
+    "https://www.reddit.com/api/v1/authorize"
+
+
 view : Model -> Html Msg
 view model =
+    let
+        authLink =
+            model.redditAuthState
+                |> Maybe.map buildAuthLinkQueryString
+                |> Maybe.map
+                    (\queryParams ->
+                        a
+                            [ Attr.href (authUrl ++ queryParams) ]
+                            [ text "authorize site to read reddit history" ]
+                    )
+                |> Maybe.withDefault (text "")
+    in
     div
         []
         [ img
@@ -51,6 +108,7 @@ view model =
             ]
             []
         , text "Hello world"
+        , authLink
         ]
 
 
