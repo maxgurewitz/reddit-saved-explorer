648af51964a98a21a4acf3037391941d1aba507f
maxgurewitz <maxthegeek1@gmail.com>
2018-10-08 10:48:20 -0700

    conditionally cache reddit code

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,25 @@
-module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
+port module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
+import Array
 import Browser
 import Dict
 import Html exposing (Html, a, div, img, text)
 import Html.Attributes as Attr
+import Json.Encode
+import Maybe.Extra
+import QS
 import Random
 import Random.Char
 import Random.String
+import Url exposing (Url)
 import Url.Builder
+
+
+
+-- PORTS
+
+
+port cache : Json.Encode.Value -> Cmd msg
 
 
 
@@ -17,15 +29,79 @@ import Url.Builder
 type alias Model =
     { publicPath : String
     , redditAuthState : Maybe String
+    , queryParams : Dict.Dict String QS.OneOrMany
+    , isLoggedIn : Bool
     }
+
+
+getQueryParamStringValue : QS.OneOrMany -> Maybe String
+getQueryParamStringValue oneOrMany =
+    case oneOrMany of
+        QS.One one ->
+            case one of
+                QS.Str str ->
+                    Just str
+
+                _ ->
+                    Nothing
+
+        QS.Many _ ->
+            Nothing
+
+
+getQueryParamsStringValue : Dict.Dict String QS.OneOrMany -> String -> Maybe String
+getQueryParamsStringValue queryParams key =
+    Dict.get
+        key
+        queryParams
+        |> Maybe.andThen getQueryParamStringValue
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
+    let
+        queryParams =
+            QS.parse QS.config flags.queryString
+
+        getQueryParam =
+            getQueryParamsStringValue queryParams
+
+        maybeRedditAuthCode =
+            Maybe.Extra.combine
+                [ getQueryParam "code"
+                , getQueryParam "state"
+                , flags.redditAuthState
+                ]
+                |> Maybe.andThen
+                    (\passedAuthCachedState ->
+                        case passedAuthCachedState of
+                            queryCode :: queryState :: cacheState :: _ ->
+                                if queryState == cacheState then
+                                    Just queryCode
+
+                                else
+                                    Nothing
+
+                            _ ->
+                                Nothing
+                    )
+
+        cacheAuthCodeCmd =
+            maybeRedditAuthCode
+                |> Maybe.map (jsonCacheValue "redditAuthCode")
+                |> Maybe.map cache
+                |> Maybe.withDefault Cmd.none
+    in
     ( { publicPath = flags.publicPath
       , redditAuthState = Maybe.Nothing
+      , queryParams = queryParams
+      , isLoggedIn = not (Maybe.Extra.isNothing maybeRedditAuthCode)
       }
-    , Random.generate GenerateRedditAuthState (Random.String.string 15 Random.Char.english)
+    , Cmd.batch
+        [ cacheAuthCodeCmd
+        , Random.generate GenerateRedditAuthState
+            (Random.String.string 15 Random.Char.english)
+        ]
     )
 
 
@@ -45,7 +121,16 @@ update msg model =
             ( model, Cmd.none )
 
         GenerateRedditAuthState randomString ->
-            ( { model | redditAuthState = Maybe.Just randomString }, Cmd.none )
+            ( { model | redditAuthState = Maybe.Just randomString }
+            , cache (jsonCacheValue "redditAuthState" randomString)
+            )
+
+
+jsonCacheValue : String -> String -> Json.Encode.Value
+jsonCacheValue key value =
+    Json.Encode.object
+        [ ( key, Json.Encode.string value )
+        ]
 
 
 
@@ -65,7 +150,7 @@ buildAuthLinkQueryString randomString =
             [ ( "client_id", clientId )
             , ( "response_type", "code" )
             , ( "state", randomString )
-            , ( "duration", "temporary" )
+            , ( "duration", "permanent" )
             , ( "scope", "history" )
             , ( "redirect_uri", redirectUri )
             ]
@@ -89,15 +174,19 @@ view : Model -> Html Msg
 view model =
     let
         authLink =
-            model.redditAuthState
-                |> Maybe.map buildAuthLinkQueryString
-                |> Maybe.map
-                    (\queryParams ->
-                        a
-                            [ Attr.href (authUrl ++ queryParams) ]
-                            [ text "authorize site to read reddit history" ]
-                    )
-                |> Maybe.withDefault (text "")
+            if model.isLoggedIn then
+                text ""
+
+            else
+                model.redditAuthState
+                    |> Maybe.map buildAuthLinkQueryString
+                    |> Maybe.map
+                        (\queryParams ->
+                            a
+                                [ Attr.href (authUrl ++ queryParams) ]
+                                [ text "authorize site to read reddit history" ]
+                        )
+                    |> Maybe.withDefault (text "")
     in
     div
         []
@@ -123,6 +212,9 @@ subscriptions model =
 
 type alias Flags =
     { publicPath : String
+    , queryString : String
+    , redditAuthState : Maybe String
+    , redditAuthCode : Maybe String
     }
 
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ require('./index.scss')
 
 var Elm = require('./Main.elm').Elm;
 
+var cache = JSON.parse(localStorage.getItem('cache') || '{}' );
+
 var app = Elm.Main.init({
   node: document.getElementById('main'),
   flags: {
-    publicPath: __webpack_public_path__
+    publicPath: __webpack_public_path__,
+    queryString: window.location.search,
+    redditAuthCode: cache.redditAuthCode || null,
+    redditAuthState: cache.redditAuthState || null,
   }
+});
+
+app.ports.cache.subscribe(function(data) {
+  localStorage.setItem('cache', JSON.stringify(data));
 });
